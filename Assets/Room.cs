36e61c9536dfc9c6c1e9d9c3d979d3a09e904292
Ben Stewart <ben.stewart@enlighten.co.nz>
2019-01-25 23:45:10 +1300

    Getting started on building rooms

---
+++ Assets/Room.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+	public List<Vector2> availableSpaces;
+	public List<Vector2> roomSpaces = new List<Vector2>() { new Vector2(0, 0), new Vector2(1, 0), new Vector2(2, 0), new Vector2(3, 0) };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+		availableSpaces = roomSpaces.Select(x => x + (Vector2)gameObject.transform.position).ToList();
+		foreach (var space in availableSpaces)
+		{
+			Debug.Log(space);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public List<Vector2> occupiedSpaces()
+	{
+		return new List<Vector2>();
+	}
+}
