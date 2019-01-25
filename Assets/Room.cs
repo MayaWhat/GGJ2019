4e6de16064074811ff64657d409d1018006d93d6
Ben Stewart <ben.stewart@enlighten.co.nz>
2019-01-26 00:14:15 +1300

    Zac was here.

--- Assets/Room.cs
+++ Assets/Room.cs
@@ -8,8 +8,15 @@ public class Room : MonoBehaviour
 	public List<Vector2> availableSpaces;
 	public List<Vector2> roomSpaces = new List<Vector2>() { new Vector2(0, 0), new Vector2(1, 0), new Vector2(2, 0), new Vector2(3, 0) };
 
-    // Start is called before the first frame update
-    void Start()
+	public List<Door> doors = new List<Door>() { new Door(new Vector2(0,0), true) };
+	public List<Stair> stairs = new List<Stair>() { new Stair(new Vector2(0, 0), true) };
+
+
+
+
+
+	// Start is called before the first frame update
+	void Start()
     {
 
 
@@ -24,10 +31,28 @@ public class Room : MonoBehaviour
     void Update()
     {
         
-    }
-
-	public List<Vector2> occupiedSpaces()
+    }	
+}
+public class Door
+{
+	public Door(Vector2 pos, bool left)
 	{
-		return new List<Vector2>();
+		position = pos;
+		isLeft = left;
 	}
+
+	public Vector2 position;
+	public bool isLeft;
+}
+
+public class Stair
+{
+	public Stair(Vector2 pos, bool up)
+	{
+		position = pos;
+		isUp = up;
+	}
+
+	public Vector2 position;
+	public bool isUp;
 }
---
+++ Assets/RoomManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+	private List<Room> _rooms;
+    // Start is called before the first frame update
+    void Start()
+    {
+		_rooms = FindObjectsOfType<Room>().ToList();
+    }
+
+	public void AddRoom(Room room)
+	{
+		_rooms.Add(room);
+	}
+
+	public Room GetRoomAtPosition(Vector2 pos)
+	{
+		var room = _rooms.FirstOrDefault(x => x.roomSpaces.Contains(pos));
+
+		return room;
+	}
+
+	public bool IsDoorAtPosition(Vector2 pos, bool doorIsLeft)
+	{
+		var room = GetRoomAtPosition(pos);
+
+		var door = room?.doors.FirstOrDefault(x => x.position == pos && x.isLeft == doorIsLeft);
+
+		return door != null;
+	}
+
+	public bool IsStairAtPosition(Vector2 pos, bool stairIsUp)
+	{
+		var room = GetRoomAtPosition(pos);
+
+		var stair = room?.stairs.FirstOrDefault(x => x.position == pos && x.isUp == stairIsUp);
+
+		return stair != null;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+}
